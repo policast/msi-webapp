6d82ed9966caf9a975cde20c36ba698aefe7a171
Enes <mail@enesimran.net>
2024-09-28 10:28:58 +0200

    made tags variable

---
+++ index.js
@@ -0,0 +1,45 @@
+function fillTags() {
+    const tags = [
+        "Sport",
+        "Gesundheit",
+        "Technik",
+        "Wissenschaft",
+        "Politik",
+        "Kultur",
+        "Wirtschaft",
+        "Musik",
+        "Kunst",
+        "Geschichte",
+        "Bildung",
+        "Reisen",
+        "Essen"
+    ];
+    const tagsHTML = document.getElementById('tags');
+    for (var i = 0; i < tags.length; i++) {
+        const tag = document.createElement('input');
+        tag.type = 'checkbox';
+        tag.className = 'btn-check';
+        tag.id = `btn-check-${i}`;
+        tag.autocomplete = 'off';
+
+        const label = document.createElement('label');
+        label.className = 'btn btn-primary';
+        label.htmlFor = tag.id;
+        label.textContent = tags[i];
+
+        tagsHTML.appendChild(tag);
+        tagsHTML.appendChild(label);
+    }
+}
+
+function fetchPodcastFiles() {
+    console.info("Podcast Files werden heruntergeladen");
+}
+
+function getPersonalizedPodcast() {
+    const listenButton = document.getElementById('listenButton');
+    listenButton.disabled = true;
+    alert("Dein Podcast wird generiert");
+    window.location.href = "podcast.html";
+}
+

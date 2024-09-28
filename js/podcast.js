a2245af015c32f71bba6ce49fcc70d3591ce7f2d
Enes <mail@enesimran.net>
2024-09-28 16:07:02 +0200

    current state

--- js/podcast.js
+++ js/podcast.js
@@ -7,3 +7,28 @@ function playPodcast() {
     podCastPlayer.classList.remove('d-none');
     themes.classList.add('centertext');
 }
+
+function buildAudioOrder() {
+    const selectedTags = JSON.parse(localStorage.getItem('selectedTags')) || [];
+    const audioOrder = [];
+
+    if (selectedTags.length > 0) {
+        let i = 0;
+        audioOrder.push("audio/welcome_" + selectedTags[i] + ".mp3");
+        audioOrder.push("audio/" + selectedTags[i] + ".mp3");
+        i++;
+
+        while(i < selectedTags.length) {
+            audioOrder.push("audio/" + selectedTags[i] + ".mp3");
+            i++;
+        }
+
+        audioOrder.push("audio/Abschluss.mp3");
+        audioOrder.push("audio/Outro.mp3");
+    } else {
+
+    }
+    console.info(audioOrder);
+    return audioOrder;
+    
+}
